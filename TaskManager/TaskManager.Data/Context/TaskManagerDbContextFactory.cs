1e4ed6b8ed134879a7c95bc55f51b3b7737d46a1
WelderCosta <79428274+WelderCosta@users.noreply.github.com>
2023-08-18 07:39:35 -0300

    Design do migration

---
+++ TaskManager/TaskManager.Data/Context/TaskManagerDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace TaskManager.Data.Context
+{
+    public class TaskManagerDbContextFactory : IDesignTimeDbContextFactory<TaskManagerDbContext>
+    {
+        public TaskManagerDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<TaskManagerDbContext>();
+            optionsBuilder.UseSqlServer("Data Source=WMC-PC\\SQLEXPRESS;Initial Catalog=DB_AprendizadoTS;User ID=sa;Password=admin123;Encrypt=False");
+
+            return new TaskManagerDbContext(optionsBuilder.Options);
+        }
+    }
+}
